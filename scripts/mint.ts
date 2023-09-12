f592b030f197c98c40f6c7dc18458d70ef77385b
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-09-12 19:30:33 +0300

    Fix mint.ts

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -3,8 +3,6 @@ import { Book, Book__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const MINT_VALUE = ethers.parseUnits("1");
-
 function setupProvider(){
   const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
   return provider;
@@ -18,14 +16,11 @@ async function main() {
     
   // Attach
   const bookContractFactory = new Book__factory(signer);
-  const bookContract = bookContractFactory.attach(process.env.VOTING_TOKEN_ADDRESS ?? "") as Book;
+  const bookContract = bookContractFactory.attach("0x69bfABd8de278B2ab3EC480175924a7C49146194") as Book;
 
   // Mint some tokens
   const mintTx = await bookContract.mint("ipfs://bafybeiffapvkruv2vwtomswqzxiaxdgm2dflet2cxmh6t4ixrgaezumbw4", ["random"]);
   await mintTx.wait();
-  console.log(`Minted ${MINT_VALUE.toString()} decimal units to account ${signer.address}.\n`);
-  const balanceBN = await bookContract.balanceOf(signer.address);  
-  console.log(`Account ${signer.address} has ${balanceBN.toString()} decimal units of VoteToken.\n`);
 }
 
 main().catch((error) => {
